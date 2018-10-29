844f822618dcca55a301837736890c1884c7fabe
snailvs2 <a1234567>
2018-10-30 02:34:29 +0800

    xixi 2:34:29.49

--- push-to-github2.bat
+++ push-to-github2.bat
@@ -24,7 +24,7 @@ exit
 ::开始复制到对应目录
 :COPY
 echo copy and git.add.commit.push
-echo "del %wwwdir%"
+echo 1--"del %wwwdir%"
 del /f/s/q %wwwdir%\app\* 1>nul 2>nul
 del /f/s/q %wwwdir%\w\* 1>nul 2>nul
 rd /s/q %wwwdir%\app\
@@ -35,18 +35,18 @@ echo push-to-github.bat>>exclude.txt
 echo push-to-github2.bat>>exclude.txt
 echo exclude.txt>>exclude.txt
 
-echo coyp...
+echo 2--coyp...
 xcopy /y/e/s/h/q . %wwwdir% /exclude:exclude.txt
 del exclude.txt
 echo "copy  finish %wwwdir%"
 
-echo git add...
+echo 3--git add...
 git --git-dir="%wwwdir%\.git" add -A
-echo git commit...
-git --git-dir="%wwwdir%\.git" commit -m"%time%"
-echo git push
+echo 4--git commit...
+git --git-dir="%wwwdir%\.git" commit -m"xixi%time%"
+echo 5--git push
 git --git-dir="%wwwdir%\.git" push
-echo git finish
+echo 6--git finish
 
 pause
 exit
